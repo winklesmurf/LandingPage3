a6e193c8497def674dbc6d6f0aeda0c6b4959b69
winklesmurf <74035176+winklesmurf@users.noreply.github.com>
2020-12-26 21:21:51 -0500

    Add files via upload

---
+++ 2-Landing-Page/js/app.js
@@ -0,0 +1,64 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+*/
+
+/**
+ * Define Global Variables
+ *
+*/
+const navMenu = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('section');
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+*/
+
+// build the nav
+function createNav(){
+for (let section of sections) {
+  let section = document.createElement('li');
+  section.className = 'navbar__link';
+  section.innerText = section.h2;
+  navMenu.appendChild(section);
+}
+};
+createNav();
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+*/
+
+// Build menu
+
+// Scroll to section on link click
+
+// Set sections as active
